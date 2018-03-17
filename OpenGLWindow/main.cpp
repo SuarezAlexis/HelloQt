022663bd3673fe3b853defb1d0717c33dd0f54d9
Jainjo <Jainjo@HEWLETTWHITE>
2018-03-16 19:38:49 -0600

    Comentarios

--- OpenGLWindow/main.cpp
+++ OpenGLWindow/main.cpp
@@ -57,24 +57,25 @@
 
 #include <QtCore/qmath.h>
 
+//Clase TriangleWindow es la ventana OpenGL
 class TriangleWindow : public OpenGLWindow
 {
 public:
-	TriangleWindow();
+	TriangleWindow(); //Constructor de la clase
 
-	void initialize() override;
-	void render() override;
+	void initialize() override;	//Métodos para inicializar y renderizar ventana,
+	void render() override;		// heredados de superclase. Se implementan más abajo.
 
 private:
-	GLuint m_posAttr;
-	GLuint m_colAttr;
-	GLuint m_matrixUniform;
+	GLuint m_posAttr;		// Variables que usarán los programas de shader
+	GLuint m_colAttr;		// guardan la ubicación de los atributos
+	GLuint m_matrixUniform;	// color y posición.
 
-	QOpenGLShaderProgram *m_program;
-	int m_frame;
+	QOpenGLShaderProgram *m_program; //Variable que apunta al programa shader
+	int m_frame; //Contador de frames para regular animación del triángulo
 };
 
-TriangleWindow::TriangleWindow()
+TriangleWindow::TriangleWindow() 
 	: m_program(0)
 	, m_frame(0)
 {
@@ -82,21 +83,22 @@ TriangleWindow::TriangleWindow()
 
 int main(int argc, char **argv)
 {
-	QGuiApplication app(argc, argv);
+	QGuiApplication app(argc, argv); //inicializar aplicación
 
-	QSurfaceFormat format;
-	format.setSamples(16);
+	QSurfaceFormat format; // Formato para definir antialiasing multi-muestra
+	format.setSamples(16); // de 4 muestras
 
-	TriangleWindow window;
-	window.setFormat(format);
+	TriangleWindow window; //Instancia de la ventana OpenGL
+	window.setFormat(format); 
 	window.resize(640, 480);
 	window.show();
 
-	window.setAnimating(true);
+	window.setAnimating(true); //Se habilita animación
 
 	return app.exec();
 }
 
+//Programas shader, son constantes globales tipo cadena
 static const char *vertexShaderSource =
 "attribute highp vec4 posAttr;\n"
 "attribute lowp vec4 colAttr;\n"
@@ -113,8 +115,8 @@ static const char *fragmentShaderSource =
 "   gl_FragColor = col;\n"
 "}\n";
 
-void TriangleWindow::initialize()
-{
+void TriangleWindow::initialize() //Implementación del método heredado de OpenGLWindow
+{ //Carga los programas de shader y guarda la ubicación de los atributos posición y color y de la matriz uniforme
 	m_program = new QOpenGLShaderProgram(this);
 	m_program->addShaderFromSourceCode(QOpenGLShader::Vertex, vertexShaderSource);
 	m_program->addShaderFromSourceCode(QOpenGLShader::Fragment, fragmentShaderSource);
@@ -124,46 +126,58 @@ void TriangleWindow::initialize()
 	m_matrixUniform = m_program->uniformLocation("matrix");
 }
 
-void TriangleWindow::render()
-{
-	const qreal retinaScale = devicePixelRatio();
-	glViewport(0, 0, width() * retinaScale, height() * retinaScale);
+void TriangleWindow::render() //Implementación del método heredado de OpenGLWindow.
+{ //Aquí se hace el renderizado, es donde se ocupa OpenGL.
+	const qreal retinaScale = devicePixelRatio(); //Se obtiene la relación entre 
+												  //las coordenadas utiles estándar y 
+												  //los pixeles reales de la pantalla.
+	glViewport(0, 0, width() * retinaScale, height() * retinaScale); //Se establece el viewport.
 
-	glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT); //Se limpia el color de fondo.
 
-	m_program->bind();
+	m_program->bind(); // Se ligan los programas de shader.
 
 	QMatrix4x4 matrix;
-	matrix.perspective(60.0f, 4.0f / 3.0f, 0.1f, 100.0f);
-	matrix.translate(0, 0, -2);
-	matrix.rotate(100.0f * m_frame / screen()->refreshRate(), 0, 1, 0);
+	matrix.perspective(60.0f, 4.0f / 3.0f, 0.1f, 100.0f); //Matriz de perspectiva.
+	matrix.translate(0, 0, -2); // Se aplica traslación
+	matrix.rotate(100.0f * m_frame / screen()->refreshRate(), 0, 1, 0); //Se aplica rotación variable (eje y)
+																		//en función de la tasa de refresco
+																		//de la pantalla.
+	//Se asigna valor uniforme obtenido en el shader de vertices.
+	m_program->setUniformValue(m_matrixUniform, matrix); 
 
-	m_program->setUniformValue(m_matrixUniform, matrix);
-
+	//Los tres vértices del triángulo.
 	GLfloat vertices[] = {
 		0.0f, 0.707f,
 		-0.5f, -0.5f,
 		0.5f, -0.5f
 	};
 
+	//Los colores para cada vértice.
 	GLfloat colors[] = {
 		1.0f, 0.0f, 0.0f,
 		0.0f, 1.0f, 0.0f,
 		0.0f, 0.0f, 1.0f
 	};
 
+	// Especifica qué atributo del vértice se modificará (en este caso la posición y abajo el color),
+	// el número de componentes por atributo, el tipo de dato de cada elemento del arreglo,
+	// si se deben o no normalizar los datos de punto fijo,
+	// el espacio en bytes entre el inicio de cada atributo de vértice consecutivo (0 significa que son contiguos),
+	// y el apuntador al arreglo de datos.
 	glVertexAttribPointer(m_posAttr, 2, GL_FLOAT, GL_FALSE, 0, vertices);
 	glVertexAttribPointer(m_colAttr, 3, GL_FLOAT, GL_FALSE, 0, colors);
 
-	glEnableVertexAttribArray(0);
-	glEnableVertexAttribArray(1);
+	glEnableVertexAttribArray(0); //Habilita los arreglos de atributos de vértice 0 y 1
+	glEnableVertexAttribArray(1); //para el color y la posición.
 
-	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glDrawArrays(GL_TRIANGLES, 0, 3); //Dibuja los arreglos de atributos de vértice habilitados 
+									  //desde el indice 0, con 3 valores por elemento.
 
-	glDisableVertexAttribArray(1);
+	glDisableVertexAttribArray(1); //Deshabilita los arreglos de atrbutos de vértice.
 	glDisableVertexAttribArray(0);
 
-	m_program->release();
+	m_program->release(); //Libera los programas de shader.
 
-	++m_frame;
+	++m_frame; //Se incrementa el contador que controla el giro del triángulo.
 }
